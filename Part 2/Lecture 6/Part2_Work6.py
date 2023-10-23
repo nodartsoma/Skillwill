7be8c3500371f7c85278221277214017f2877fea
nodartsoma <Nodara050@gmail.com>
2023-10-24 00:41:24 +0400

    Initial commit

---
+++ Part 2/Lecture 6/Part2_Work6.py
@@ -0,0 +1,43 @@
+class Person:
+    def __init__(self):
+        self.heart = Heart
+        self.brain = Brain
+
+
+class Heart:
+    def __init__(self, usage):
+        self.usage = usage
+
+    @property
+    def state(self):
+        if self.usage > 70:
+            return "high blood pressure"
+        return "feeling good"
+
+
+class Brain:
+    def __init__(self, usage):
+        self.usage = usage
+
+    @property
+    def state(self):
+        if self.usage > 90:
+            return "tired"
+        return "rested"
+
+
+class Leg:
+    def __init__(self, moving_speed):
+        self.mov_spd = moving_speed
+
+    @property
+    def moving_speed(self):
+        if self.mov_spd > 10:
+            return "running"
+        elif self.mov_spd > 0:
+            return "walking"
+        return "standing"
+
+
+person = Person()
+person.leg = Leg(11)
