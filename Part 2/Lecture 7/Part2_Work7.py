6d576a160a580dc74580e57fe8624bae89757448
nodartsoma <Nodara050@gmail.com>
2023-10-24 23:38:13 +0400

    Initial commit

---
+++ Part 2/Lecture 7/Part2_Work7.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+
+class Car(Vehicle):
+    def __init__(self, max_speed=240, current_speed=0):
+        self.max_speed = max_speed
+        self.current_speed = current_speed
+
+    def start_engine(self):
+        return "car started"
+
+    def stop_engine(self):
+        return "car stopped"
+
+
+class SportCar(Car):
+    def start_engine(self):
+        print(super().start_engine())
+        return Car().max_speed
+
+    def stop_engine(self):
+        print(super().stop_engine())
+        Car().current_speed = 0
+
+
+SportCar().start_engine()
