0c27a8e62dec295601a75dfaa2ae816ceb267906
nodartsoma <Nodara050@gmail.com>
2023-12-03 23:51:59 +0400

    Initial commit

---
+++ MyCalendar/Calendar/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+today = datetime.now()
+date_of_birth = models.DateField()
+
+
+class CalendarClass(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    age = today.year - int(date_of_birth)
