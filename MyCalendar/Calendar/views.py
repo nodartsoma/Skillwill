30ef3364d7ba80ad68c47a8104ad0be8f30a3b42
nodartsoma <Nodara050@gmail.com>
2023-12-04 00:28:51 +0400

    Initial commit

--- MyCalendar/Calendar/views.py
+++ MyCalendar/Calendar/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-import MyCalendar.Calendar.models
 
 
 def calendar_index():
-    return HttpResponse(MyCalendar.Calendar.models.CalendarClass)
+    return HttpResponse('Hope it worked...')
--- MyCalendar/User/views.py
+++ MyCalendar/User/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-import MyCalendar.User.models
-
 
 def user_index():
-    return HttpResponse(MyCalendar.User.models.UserClass)
+    return HttpResponse('It worked!! God damnit')
